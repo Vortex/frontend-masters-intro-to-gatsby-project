3ca7ac80a432c29ab89857ab30a42a257476e9f0
Peco Danajlovski <pecod@sourcico.com>
2021-12-10 22:52:51 +0100

    add site metadata

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteMetadata: {
+        siteUrl: 'https://www.yourdomain.tld',
+        title: 'Frontend Masters Intro to Gatsby',
+        description: 'Frontend Masters Intro to Gatsby course',
+        image: 'https://kanbanzone.com/wp-content/uploads/2021/04/kanban-zone-ikigai-japanese-concept.png'
+    }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,28 @@
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import * as React from 'react';
 
 export default function IndexPage() {
-    return (
-        <main>
-            <h1>Hello frontend masters</h1>
-        </main>
-    );
+  const data = useStaticQuery(graphql`
+    query GetSiteTitle {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  const meta = data?.site?.siteMetadata ?? {};
+
+  return (
+    <>
+      <header>
+          <Link to="/">{meta.title}</Link>
+      </header>
+      <main>
+        <h1>Hello frontend masters</h1>
+        <Link to="/about">About this page</Link>
+      </main>
+    </>
+  );
 }

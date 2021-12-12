508a3c978221c292fba819a090322b1f45fb8ce0
Peco Danajlovski <pecod@sourcico.com>
2021-12-12 19:07:51 +0100

    introduced seo component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
+import { Layout } from '../components/layout';
 
 export default function IndexPage() {
-    return (
-        <main>
-            <h1>About this site!</h1>
-            <Link to="/">Go home</Link>
-        </main>
-    );
+  return (
+    <>
+      <Layout title="About this page" description="Description about this page" >
+        <h1>About this site!</h1>
+        <Link to="/">Go home</Link>
+      </Layout>
+    </>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,15 @@
-import { Link, useStaticQuery, graphql } from 'gatsby';
+import { Link } from 'gatsby';
 import * as React from 'react';
+import { Layout } from '../components/layout';
 
 export default function IndexPage() {
-  const data = useStaticQuery(graphql`
-    query GetSiteTitle {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
-  const meta = data?.site?.siteMetadata ?? {};
 
   return (
     <>
-      <header>
-          <Link to="/">{meta.title}</Link>
-      </header>
-      <main>
-        <h1>Hello frontend masters</h1>
-        <Link to="/about">About this page</Link>
-      </main>
+        <Layout>
+            <h1>Hello frontend masters</h1>
+            <Link to="/about">About this page</Link>
+        </Layout>
     </>
   );
 }
